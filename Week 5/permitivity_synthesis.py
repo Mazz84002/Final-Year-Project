9e2eb1d55e382f6b5a506107f6ae1e639689a142
Mazz84002 <93880850+Mazz84002@users.noreply.github.com>
2023-05-20 15:53:01 +0300

    fimalising

---
+++ Week 5/permitivity_synthesis.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    L = 200e-6
+    W = 20e-6
+    S = W
+    Lstrip = 3e-6
+    Sstrip = 1e-6
+    Wstrip = 30e-6
+    Rg = 50 # omega
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
